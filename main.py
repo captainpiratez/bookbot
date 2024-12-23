dcaa307053bb6ffae5d32f5d258d489c7c77401d
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2024-12-24 00:49:48 +0300

    implemented counting characters

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
-
+    print(count_characters(text))
 
 def get_book_text(path):
     with open(path) as f:
@@ -14,5 +14,14 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+def count_characters(text):
+    lowered_text = text.lower()
+    analyzed_text = {}
+    for i in lowered_text:
+        if i in analyzed_text:
+            analyzed_text[i] += 1
+        else:
+            analyzed_text[i] = 1
+    return analyzed_text
 
 main()
