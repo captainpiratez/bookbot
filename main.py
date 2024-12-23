9b18e7d2d91729f8f10ee2b52f83ff8a20da9d9b
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2024-12-24 01:06:41 +0300

    fixed get char dict and added beginning of get sorted dict

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(count_characters(text))
+    print(chars_dict)
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -14,14 +17,21 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def count_characters(text):
-    lowered_text = text.lower()
+
+def get_chars_dict(text):
     analyzed_text = {}
-    for i in lowered_text:
-        if i in analyzed_text:
-            analyzed_text[i] += 1
-        else:
-            analyzed_text[i] = 1
+    for i in text:
+        lowered = i.lower()
+        if lowered.isalpha():
+            if lowered in analyzed_text:
+                analyzed_text[lowered] += 1
+            else:
+                analyzed_text[lowered] = 1
     return analyzed_text
+
+
+def get_sorted_dict(dict):
+    return dict.sort()
+
 
 main()
